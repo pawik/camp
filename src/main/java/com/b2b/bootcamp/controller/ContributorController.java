d1527f9eb8cb9f7fe592eec3b166c68e49060114
Imie Nazwisko <email@a.com>
2018-01-03 21:23:39 +0100

    delete duplicate

--- src/main/java/com/b2b/bootcamp/controller/ContributorController.java
+++ src/main/java/com/b2b/bootcamp/controller/ContributorController.java
@@ -5,7 +5,6 @@ import com.b2b.bootcamp.service.ContributorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
