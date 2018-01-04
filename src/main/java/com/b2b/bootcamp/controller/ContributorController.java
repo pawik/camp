d287656786091dca92f929f2dc17856ec8895f20
b2bnetworkwarszawa <32678702+b2bnetworkwarszawa@users.noreply.github.com>
2018-01-04 19:31:54 +0100

    Test weronika (#2)
    * delete duplicate
    
    * ssss

--- src/main/java/com/b2b/bootcamp/controller/ContributorController.java
+++ src/main/java/com/b2b/bootcamp/controller/ContributorController.java
@@ -19,6 +19,8 @@ public class ContributorController {
         List<Contributor> anything = service.findAnything();
         model.setViewName("listOdAnything");
         model.addObject("list", anything);
+        System.out.println("duplicate");
+        System.out.println("cos");
         return model;
     }
 
